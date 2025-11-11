7a1688c79c3da50782e45bba3eb0f41f0d6cd830
Gregg Trunnell <gggtrunnell@gmail.com>
2025-11-11 15:26:40 -0600

    added some styling to Nav

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
+import './Nav.css'
 function Nav() {
 
   return (
     <div className='Nav'>
-      <NavLink to="/feedbackForm">
-        feedbackForm
+      <NavLink to="/feedbackForm" className="nav-button">
+        Feedback Form 
       </NavLink>
-      <NavLink to="/feedbackList">
-        feedbackList
+      <NavLink to="/feedbackList" className="nav-button">
+        Feedback List
       </NavLink>
     </div>
   );
